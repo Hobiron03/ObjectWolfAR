3c1ecf09c9e36ca13929a21371a8353aa20b8461
HOBIRON <yudainepnep@icloud.com>
2020-12-30 21:39:03 +0900

    ADD fadeout function WIP

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,11 +9,12 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject selectHowToPlayUI;
+    public GameObject fadePanel;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Image fadePanelImage = fadePanel.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -30,8 +33,25 @@ public class GameManager : MonoBehaviour
         selectHowToPlayUI.SetActive(false);
     }
 
+
     #region SceneTransition
 
+    public void FadeOutScreen(string destination)
+    {
+
+
+        SceneManager.LoadScene(destination);
+    }
+
+    //一つの端末でプレイ
+    public void GoToOnePhoneGameScreen()
+    {
+        FadeOutScreen("OnePhoneGame");
+    }
+    public void GoToMultiPhoneGameScreen()
+    {
+        SceneManager.LoadScene("OnePhoneGame");
+    }
     public void GoToMuseum()
     {
         SceneManager.LoadScene("Museum");
